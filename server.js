ffdfc80807295adcb64de5548eb6bf44fc8274df
Geovani Brito <geovani.br@gmail.com>
2021-09-17 15:18:39 -0300

    feat: rota para teste de velocidade

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 const express = require('express')
-const speedtest = require('./speedtest')
-const router = require('./api/router')
+const speedtestRouter = require('./api/router/speedtest-router')
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use('/api/v1/speedtest', router)
-
-/* app.get('/api/mensagem', (req, res) => {
-  res.send({ msg: 'Hello From Express' });
-});
-
-app.get('/api/v1/speedtest', (req, res) => {
-  const test_result = speedtest()
-  res.send(test_result)
-})*/
+app.use('/api/v1/speedtest', speedtestRouter)
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

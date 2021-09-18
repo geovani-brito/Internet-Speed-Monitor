e4ed13c7e8b2c80683992e494fa27cf71f54d3d4
Geovani Brito <geovani.br@gmail.com>
2021-09-18 12:59:02 -0300

    feat: teste da API no frontend

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const port = process.env.PORT || 5000;
 
 app.use('/api/v1/speedtest', speedtestRouter)
 
+app.get('/api/v1/mensagem', (req, res) => {
+    res.send({"msg": "front-end ok"}).json
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
